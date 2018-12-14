4856d4f24fd102d2f5d4efc189b88984e31504a1
vittes <vitte@bk.ru>
2018-12-14 23:54:20 +0300

    Создания авторизации в меню сайта final

--- templates/menu.php
+++ templates/menu.php
@@ -8,7 +8,7 @@ require_once ('../engine/menu_builder.php');
 
  if($user){
 echo "<a href='#'>".$user['user_name']."</a>";
-echo "<a href='#'>Выйти</a>";
+echo "<a href='exit.php'>Выйти</a>";
 //unset($user['user_name']);
 }
  else{
