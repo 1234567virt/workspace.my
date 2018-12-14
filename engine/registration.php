6606184e7efbe06949d002fcb90e849a44aaece4
vittes <vitte@bk.ru>
2018-12-14 14:08:56 +0300

    registration1.0final

--- engine/registration.php
+++ engine/registration.php
@@ -1,16 +1,20 @@
 <?php
  require_once '../engine/init.php';
 function registration($link,$login,$password,$name,$call){
-  $login=clear($link,$login);
-  $password=clear($link,$password);
-  $name=clear($link,$name);
-  $call=clear($link,$call);
-
-   mysqli_query($link ,"INSERT INTO `user`( `user_name`, `user_login`, `user_password`, `u_call`)
-         VALUES
-        ('$login','$password','$name','$call')");
-      header("Location:../public_html/logout.php");
+  if(isset($login) && isset($password) && isset($name) && isset($call)){
+    $login=clear($link,$login);
+    $password=clear($link,$password);
+    $name=clear($link,$name);
+    $call=clear($link,$call);
+    $password=password_hash ($password, PASSWORD_BCRYPT);
+    mysqli_query($link ,"INSERT INTO `user`( `user_name`, `user_login`, `user_password`, `u_call`)
+          VALUES
+          ('$name','$login','$password','$call')");
+        header("Location:../public_html/logout.php");
     }
-
+    else{
+      echo "Ошибка пустые данные";
+    }
+}
   registration($link,$_POST['login'],$_POST['password'],$_POST['name'],$_POST['u_call']);
  
