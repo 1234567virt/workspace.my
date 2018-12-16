d057eea56a8a3130911fc7902df4ff8a6b2f65b9
vittes <vitte@bk.ru>
2018-12-16 15:23:24 +0300

    новый шаблон главной страницы готов фон и заголовок и меню

---
+++ public_html/index1.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php
+$title="Главная";
+$h1="Санрайс";
+$year=date("Y");
+
+?>
+    <head>
+    <style>
+   
+   #templatemo_body{
+background: url(img/templatemo_body.jpg) repeat;
+}
+#templatemo_body_wrapper {
+
+background: url(img/templatemo_body_top.jpg) repeat-x top
+}
+#templatemo_header {
+background: url(img/templatemo_header_bg.png) no-repeat bottom; 
+}
+
+#templatemo_menubar {
+
+background: url(img/templatemo_menubar.png) no-repeat;
+}
+
+#templatemo_main {
+background: url(img/templatemo_main_bg.png) repeat-y;
+}
+
+    </style>
+        <meta charset="UTF-8">
+        <title><?php echo $title ?></title>
+        <link rel="stylesheet" href="./css/menu.css">
+         <!-- <link rel="stylesheet" href="./css/style.css">  -->
+                  <link rel="stylesheet" href="./css/header.css"> 
+    </head>
+    <body id='templatemo_body' >
+    <div id="templatemo_body_wrapper">
+        <div id="templatemo_wrapper">
+     
+             <div id="templatemo_header">
+                    <div id="site_title"><h1><a href="#">Санрайс</a></h1></div>
+                        <div class="cleaner"></div>
+                 </div> <!-- END of templatemo_header -->
+
+                             <div id="templatemo_menubar">
+
+                                         <div id="top_nav" class="ddsmoothmenu">
+                                            <ul>
+                                                    <li><a href="index.html">Home</a></li>
+                                                    <li><a href="products.html">Products</a>
+                                                
+                                                    </li>
+                                                    <li><a href="about.html" class="selected">About</a>
+                                                
+                                                    </li>
+                                                    <li><a href="faqs.html">FAQs</a></li>
+                                                    <li><a href="checkout.html">Checkout</a></li>
+                                                    <li><a href="contact.html">Contact Us</a></li>
+                                                </ul>
+                                            <br style="clear: left" />
+                                         </div>
+
+                             </div>
+
+    </div>
+    </div>
+</body>
+</html>
+
