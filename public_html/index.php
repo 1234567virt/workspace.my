59dbc754d59a137a9d8e3158c764f6f40529b91a
vittes <vitte@bk.ru>
2018-12-17 18:53:59 +0300

    index-slider

--- public_html/index.php
+++ public_html/index.php
@@ -32,6 +32,13 @@ background: url(img/templatemo_main_bg.png) repeat-y;
     </style>
         <meta charset="UTF-8">
         <title><?php echo $title ?></title>
+        <link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="nivo-slider.css" type="text/css" media="screen" />
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js">
+
+</script>
         <link rel="stylesheet" href="./css/menu.css">
          <!-- <link rel="stylesheet" href="./css/style.css">  -->
                   <link rel="stylesheet" href="./css/header.css"> 
@@ -55,7 +62,24 @@ background: url(img/templatemo_main_bg.png) repeat-y;
                                          </div>
 
                              </div>
-
+                             <div id="slider-wrapper">
+                <div id="slider" class="nivoSlider">
+                    <img src="img/nout2.jpg" alt="" title="" />
+                    <a href="#"><img src="img/nout1.jpg" alt="" /></a>
+                    <img src="img/nout3.jpg" alt="" />
+                    <img src="img/nout4.jpg" alt="" title="#htmlcaption" />
+                </div>
+                <div id="htmlcaption" class="nivo-html-caption">
+                      
+                </div>
+            </div>
+            <script type="text/javascript" src="js/jquery-1.4.3.min.js"></script>
+            <script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+            <script type="text/javascript">
+            $(window).load(function() {
+                $('#slider').nivoSlider();
+            });
+            </script>
     </div>
     </div>
 </body>
