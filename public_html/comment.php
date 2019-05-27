9d80670d87aa82d020f6abd48fde15f0122f8e08
vittes <vitte@bk.ru>
2019-05-27 23:35:18 +0300

    ajax coment

---
+++ public_html/comment.php
@@ -0,0 +1,14 @@
+<?php 
+require_once '../engine/init.php';
+$sql="SELECT * FROM `otziv`";
+$massiv=mysqli_query($link,$sql);
+echo "<br>";
+while($rower = mysqli_fetch_array($massiv)){
+   echo "<hr>";
+      echo "Дата:".$rower['date']."<br>";
+      echo "Автор:".$rower['Aftor']."<br>";
+      echo "Текст:".$rower['text']."<br>";
+     
+}
+
+?>
