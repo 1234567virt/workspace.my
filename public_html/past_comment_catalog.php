d5be5f9764199cca1155ba4c9f4605e680855e5f
vittes <vitte@bk.ru>
2019-06-18 01:08:38 +0300

    class_comment_controler

--- public_html/past_comment_catalog.php
+++ public_html/past_comment_catalog.php
@@ -1,11 +1,11 @@
 <?php
 require_once('../engine/init.php');
-
+$review=new Comment();
 if(isset($_POST['marka'])){
         review_nout($link,$_POST['Aftor'],$_POST['Message'],$_POST['id'],$_POST['marka']);
 
     }
 else{
-        rewiev($link,$_POST['Aftor'],$_POST['Message'],$_POST['id']);
+       $review->rewiev($_POST['Aftor'],$_POST['Message']);
 
 }
