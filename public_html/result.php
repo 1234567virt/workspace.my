d331fd305c55c79d8aa9d3939fa6471bb856f732
Serj <krupenkosm@bk.ru>
2019-09-12 02:05:04 +0300

    error-12-9-19
    Signed-off-by: Serj <krupenkosm@bk.ru>

--- public_html/result.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../engine/init.php');
-if(isset($_POST['hero']) && isset($_POST['numberOne']) && isset($_POST['numberTwo'])){
-    $calc=new MathO();
-    $result=$calc->mathOperation($_POST['numberOne'],$_POST['numberTwo'],$_POST['hero']);
-    echo json_encode($result);
-    }
-   
-?>
