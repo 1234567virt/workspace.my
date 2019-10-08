c64cc514164411c37a11e59d47ad4052624bb105
serj <krupenkosm@bk.ru>
2019-10-08 19:35:47 +0300

    динам меню

--- templates/menu.php
+++ templates/menu.php
@@ -1,11 +1,11 @@
 <?php
  require_once ('../engine/menu_builder.php');
- 
+ //Меню Динамическое выводиться с помощью сервера
                 foreach($menu as $url=>$key){
                    
                    echo  "<li><a href='".$url."'>".$key."</a></li>";
 }
-
+//Кусок начинает выводиться только после аутентификации
                   if(isset($user)){
                      ?>
 <style>
@@ -18,13 +18,16 @@
 }
 </style>
 <?php
+//Кусочек кода выводиться если зайти как админ
                      if($user['role']=='admin'){
                         echo  "<li><a href='cms_catalog.php'>CMS</a></li>";
                      }
+                     //Конец куска
                      if(isset($_SESSION['user_id'])){
                         echo  " <a href='basket.php' style='text-decaration:none;font-size:14px;color:blue'>Личный кабинет</a><br>";
                         echo " <a href='/engine/exit.php' style='text-decaration:none;font-size:14px;color:blue'>Выйти</a>";
                       }
+                      
 }
  else{
     
